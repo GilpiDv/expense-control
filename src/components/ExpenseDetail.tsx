963eda4f5ff8ffae2c3b2d066e1d295e90286c5f
GilpiDv <gilgonzalez981@gmail.com>
2025-05-23 20:11:01 -0600

    add category filter

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -45,7 +45,7 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
                 leadingActions={leadingActions()}
                 trailingActions={trailingActions()}
             >
-                <div className="bg-white shadow-lg p-10 w-full border-b border-gray-400 flex gap-5 items-center">
+                <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex gap-5 items-center">
                     <div className="hidden sm:block">
                         <img src={`/icono_${categoryInfo.icon}.svg`} alt="Expense Icon" className="w-20" />
                     </div>
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -5,14 +5,16 @@ import ExpenseDetail from "./ExpenseDetail";
 export default function ExpenseList() {
     const { state } = useBudget();
 
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+    const filteredExpenses = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory) : state.expenses
+    const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses]);
 
     return (
-        <div className="mt-10">
+        <div className="bg-white shadow-lg rounded-lg p-10 mt-10">
             {isEmpty ? 
                 <p className="text-gray-600 text-2xl font-bold text-center">No Expenses</p> :   
                 <>
-                    {state.expenses.map((expense) => (
+                    <p className="text-gray-600 text-2xl font-bold text-center my-5">Expense List</p>
+                    {filteredExpenses.map((expense) => (
                         <ExpenseDetail 
                             key={expense.id}
                             expense={expense}
